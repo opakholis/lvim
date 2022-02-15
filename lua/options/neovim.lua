29eb382503daea9a10d65b32f0b399f9fd763466
Opa Kholis Majid <code@opakholis.space>
2022-02-15 17:39:42 +0700

    refactor: disable netrw

--- lua/options/neovim.lua
+++ lua/options/neovim.lua
@@ -2,6 +2,8 @@ vim.opt.wrap = true
 vim.opt.cmdheight = 1
 vim.opt.timeoutlen = 500 -- Keymap timeout
 vim.opt.relativenumber = true -- Relative row number
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.dashboard_disable_statusline = 1
 -- https://sw.kovidgoyal.net/kitty/faq/#some-special-symbols-are-rendered-small-truncated-in-kitty
 vim.cmd [[let &t_ut='']]
