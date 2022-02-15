cbda70ab97d59e937d2c06cfdc34b873b1022179
Opa Kholis Majid <code@opakholis.space>
2022-02-15 17:15:09 +0700

    feat: add editorconfig support

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -18,5 +18,6 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.notify.active = true
 
 lvim.builtin.copilot = true -- if you want microsoft to abuse your soul
+lvim.builtin.editorconfig = true -- enable/disable editorconfig
 lvim.builtin.fancy_rename = true -- fancy rename
 lvim.builtin.fancy_highlight_args = true -- fancy highlight args (CPU usage is probably not happy)
