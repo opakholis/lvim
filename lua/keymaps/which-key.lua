6c894dc9d885d23918f12c76e69aa8d2fb5b39a7
Opa Kholis Majid <code@opakholis.space>
2022-02-15 17:25:46 +0700

    feat(keymaps): set trouble mapping

--- lua/keymaps/which-key.lua
+++ lua/keymaps/which-key.lua
@@ -1,3 +1,14 @@
+-- Which-key: +Diagnostics
+lvim.builtin.which_key.mappings["d"] = {
+  name = "+Diagnotics",
+  t = { "<cmd>TroubleToggle<cr>", "Toggle Trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "Quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "Loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
+}
+
 -- Which-key: +Lsp
 if lvim.builtin.fancy_rename then
   lvim.builtin.which_key.mappings["l"]["r"] = { "<cmd>lua require('renamer').rename()<cr>", "Rename" }
