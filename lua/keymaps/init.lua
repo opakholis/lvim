61278398517fa129fd550a1174cf76f2832ada92
Opa Kholis Majid <code@opakholis.space>
2022-02-15 14:23:59 +0700

    feat(keymaps): i am too lazy to press `ggVG`

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,3 +1,7 @@
+-- LunarVim keymaps
+lvim.keys.insert_mode["jk"] = "<ESC>:w<CR>"
+lvim.keys.normal_mode["<C-a>"] = "<ESC>ggVG<CR>"
+
 -- Github Copilot
 if lvim.builtin.copilot then
   lvim.keys.insert_mode["<C-h>"] = { [[copilot#Accept("\<CR>")]], { expr = true, script = true } }
