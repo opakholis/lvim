1d694a2cbcc3798da467682a23e0744e0ce6bc02
Opa Kholis Majid <code@opakholis.space>
2022-02-28 20:57:19 +0700

    refactor(telescope): better preview_cutoff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,13 +3,13 @@ local M = {}
 -- Beautiful default layout ofro telescope prompt
 function M.layout_config()
   return {
-    width = 0.90,
+    width = 0.85,
     height = 0.85,
-    preview_cutoff = 120,
+    preview_cutoff = 100,
     prompt_position = "bottom",
     horizontal = {
       preview_width = function(_, cols, _)
-        if cols > 200 then
+        if cols < 100 then
           return math.floor(cols * 0.5)
         else
           return math.floor(cols * 0.6)
