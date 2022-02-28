6ed864494bfd226c83c20f9af42c002bffacf943
Opa Kholis Majid <code@opakholis.space>
2022-02-28 20:58:36 +0700

    refactor(zen): set width to 0.75

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -13,7 +13,7 @@ M.config = function()
       -- * an absolute number of cells when > 1
       -- * a percentage of the width / height of the editor when <= 1
       -- * a function that returns the width or the height
-      width = 0.85, -- width of the Zen window
+      width = 0.75, -- width of the Zen window
       height = 0.95, -- height of the Zen window
       -- by default, no options are changed for the Zen window
       -- uncomment any of the options below, or add other vim.wo options you want to apply
