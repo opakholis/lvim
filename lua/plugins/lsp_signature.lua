1269436d17e80e4851a5185c10d472e84304458a
Opa Kholis Majid <code@opakholis.space>
2022-04-16 19:08:23 +0700

    refactor(lsp): show completion lsp_signature

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -30,6 +30,7 @@ M.config = function()
 
     always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing
     -- auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
+    check_completion_visible = true, -- adjust position of signature window relative to completion popup
     -- extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
     zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
 
