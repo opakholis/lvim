32b6bad8ff22282009befd8b0c059f03aba9e165
Opa Kholis Majid <code@opakholis.space>
2022-04-30 05:31:45 +0700

    refactor: update disable plugins

--- lua/options/neovim.lua
+++ lua/options/neovim.lua
@@ -1,9 +1,22 @@
+local disabled_plugins = {
+  "gzip",
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "tar",
+  "tarPlugin",
+  "spellfile_plugin",
+  "zip",
+  "zipPlugin",
+}
+for _, plugin in pairs(disabled_plugins) do
+  vim.g["loaded_" .. plugin] = 1
+end
 vim.opt.wrap = true
 vim.opt.cmdheight = 1
 vim.opt.timeoutlen = 500 -- Keymap timeout
 vim.opt.relativenumber = true -- Relative row number
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.g.mkdp_auto_start = 0 -- makrdown-preview auto start
 vim.g.mkdp_browser = "google-chrome-stable"
 vim.g.dashboard_disable_statusline = 1
