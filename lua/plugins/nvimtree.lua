bc74ceff04e87ff9f91c687e54bbf6ee4e59a06c
Opa Kholis Majid <code@opakholis.space>
2022-05-13 14:14:00 +0700

    refactor(nvimtree): hide when on open file

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -7,6 +7,7 @@ M.config = function()
 
   lvim.builtin.nvimtree.setup.view.side = "left"
   lvim.builtin.nvimtree.show_icons.git = 0
+  lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
 end
 
 return M
