020fd33c8e34b212012b3ff21662675ab652ff6b
Opa Kholis Majid <code@opakholis.space>
2022-05-29 07:30:07 +0700

    refactor(motion): update cinnamon config

--- lua/plugins/cinnamon.lua
+++ lua/plugins/cinnamon.lua
@@ -7,11 +7,17 @@ M.config = function()
   end
 
   cinnamon.setup {
+    -- KEYMAPS:
     default_keymaps = true, -- Enable default keymaps.
     extra_keymaps = true, -- Enable extra keymaps.
-    extended_keymaps = true, -- Enable extended keymaps.
+    extended_keymaps = false, -- Enable extended keymaps.
+    override_keymaps = false, -- Replace any existing keymaps.
+
+    -- OPTIONS:
+    always_scroll = false, -- Scroll the cursor even when the window hasn't scrolled.
     centered = true, -- Keep cursor centered in window when using window scrolling.
     default_delay = 10, -- The default delay (in ms) between lines when scrolling.
+    horizontal_scroll = true, -- Enable smooth horizontal scrolling when view shifts left or right.
     scroll_limit = 100, -- Max number of lines moved before scrolling is skipped.
   }
 end
