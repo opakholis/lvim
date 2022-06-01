e877b6535e764a138e5d735ca13da11e864ee845
Opa Kholis Majid <code@opakholis.space>
2022-06-01 15:42:31 +0700

    refactor(treesitter): disable catppuccin ts_rainbow setting, using my own color instead

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -4,7 +4,7 @@ M.config = function()
   require("catppuccin").setup {
     integrations = {
       which_key = true,
-      ts_rainbow = true,
+      -- ts_rainbow = true,
       hop = true,
       telekasten = false,
       symbols_outline = false,
