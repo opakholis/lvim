b4899ea473d77c15d8f56c736716b0b63da325f8
Opa Kholis Majid <code@opakholis.space>
2022-07-12 06:36:59 +0700

    perf(lsp): use prettierd instead of prettier

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -56,7 +56,7 @@ local formatters_table = {
     },
   },
   {
-    command = "prettier",
+    command = "prettierd",
     filetypes = {
       "css",
       "html",
@@ -113,11 +113,11 @@ linters.setup(linters_table)
 
 -- Disable formatting capability of tsserver and jsonls
 -- as we use prettier and/or eslint_d to format/fix
--- lvim.lsp.on_attach_callback = function(client, _)
---   if client.name ~= "tsserver" and client.name ~= "jsonls" then
---     return
---   end
+lvim.lsp.on_attach_callback = function(client, _)
+  if client.name ~= "tsserver" and client.name ~= "jsonls" then
+    return
+  end
 
---   client.resolved_capabilities.document_formatting = false
---   client.resolved_capabilities.document_range_formatting = false
--- end
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
+end
