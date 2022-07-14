be738cc455f355f296f0fc35fc9773ed21d9cc2a
Opa Kholis Majid <code@opakholis.space>
2022-07-14 22:38:24 +0700

    style(treesitter): cleanup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,8 +7,6 @@ M.config = function()
 
   -- if you don't want all the parsers change this to a table of the ones you want
   lvim.builtin.treesitter.ensure_installed = "maintained"
-  lvim.builtin.treesitter.playground.enable = true
-  lvim.builtin.treesitter.highlight.enabled = true
   lvim.builtin.treesitter.autotag.enable = true
 end
 
