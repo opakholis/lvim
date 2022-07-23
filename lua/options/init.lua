7471e32bae22463b1b30492edc4d60aa11fcf751
Opa Kholis Majid <code@opakholis.space>
2022-07-23 10:26:12 +0700

    perf: disable faccy_highlight_args

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -17,4 +17,4 @@ lvim.builtin.terminal.active = true
 lvim.builtin.presence = true -- discord presence
 lvim.builtin.copilot = { active = true, cmp = false } -- if you want microsoft to abuse your soul
 lvim.builtin.editorconfig = true -- enable/disable editorconfig
-lvim.builtin.fancy_highlight_args = true -- fancy highlight args (CPU usage is probably not happy)
+lvim.builtin.fancy_highlight_args = false -- fancy highlight args (CPU usage is probably not happy)
