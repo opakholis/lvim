4949c73457326ab8a5ee800cfb007d6657e7f990
Opa Kholis Majid <code@opakholis.space>
2022-07-23 10:22:36 +0700

    refactor:colorscheme): disable notify theme for catppuccin

--- lua/themes/catppuccin.lua
+++ lua/themes/catppuccin.lua
@@ -6,12 +6,11 @@ M.config = function()
     term_colors = false,
     integrations = {
       lsp_trouble = true,
-      nvimtree = {
-        transparent_background = lvim.transparent_window,
-      },
+      nvimtree = { transparent_background = lvim.transparent_window },
       which_key = true,
       ts_rainbow = true,
       hop = true,
+      notify = false,
       telekasten = false,
       symbols_outline = false,
     },
